802fabe6b89e5f80a7efa0e3a3556a63e882806b
adityaa002 <aaeaditya02@gmail.com>
2025-03-20 22:36:19 +0530

    done

---
+++ src/com/rays/core/practice/TryFinally.java
@@ -0,0 +1,27 @@
+
+package com.rays.core.practice;
+
+public class TryFinally {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 0;
+
+		try {
+			int c = a / b;
+
+		} finally {
+			System.out.println("finally is executed");
+			// TODO: handle finally clause
+			// always executed
+		}
+
+		/*
+		 * try { int c = a / b;
+		 * 
+		 * } catch (Exception e) { System.out.println(e.getMessage()); // TODO: handle
+		 * exception } System.out.println("Caught Exception");
+		 */
+
+	}
+
+}
